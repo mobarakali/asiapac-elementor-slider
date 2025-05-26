a5b19c809b3639d9157a432db853e2c2aba25602
Mobarak Ali <mobarak.ali@gmail.com>
2025-05-26 13:27:15 +0600

    Add asiapac-elementor-slider plugin and update README.md
    - Added new file: asiapac-elementor-slider.php
    - Updated README.md with relevant information

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Asiapac-Elementor-Slider
+# Asiapac Elementor Slider
+
+Initially I have to create this slider plugin to solve a problem with the Elementor slider widget. The Elementor slider widget does not support Image with Content, so I created this plugin to allow users to create sliders using Images and Content.
+
+I have a slider desing that contains an image on the left and content on the right. I first created the design in elementor and thent saved the desing as Elemento Template. After that, I created a shortcode to display the Elementor Template in the slider widget.
+
+Thus this plugin allows users to create sliders with any custom design!
+
+## Features
+
+- Create sliders with any custom design using Elementor templates.
+- Use Elementor templates to create sliders with images and content.
+- Easy to use and customize.
+
+## Installation
+
+1. Download the plugin from the WordPress repository or upload it to your WordPress site.
+2. Activate the plugin from the WordPress admin panel.
+3. Create an Elementor template with your desired design.
+4. Use the shortcode `[asiapac_elementor_slider id="your_template_id"]` to display the slider in your posts or pages, replacing `your_template_id` with the ID of your Elementor template. Example: `[asiapac_elementor_slider ids="1371,1335"]`
+5. Customize the slider settings as needed.
+
+## Usage
+
+To use the plugin, simply create an Elementor template with your desired design and use the shortcode provided above to display the slider in your posts or pages. You can customize the slider settings in the plugin settings page.
+
+## Contributing
+
+If you would like to contribute to this plugin, please fork the repository and submit a pull request. We welcome contributions from the community to improve the plugin and add new features.
+
+## License
+
+This plugin is licensed under the GPLv2 or later license. You can view the full license text in the LICENSE file in the root of the repository.
+
+## Support
+
+If you have any questions or need support, please open an issue on the GitHub repository or contact us through the WordPress support forums. We are happy to help and will do our best to assist you with any issues you may encounter.
+
+## Changelog
+
+### 1.0.0
+
+- Initial release of the Asiapac Elementor Slider plugin.
---
+++ asiapac-elementor-slider.php
@@ -0,0 +1,109 @@
+<?php
+./**
+ * Plugin Name: Asiapac Elementor Slider
+ * Plugin URI: https://github.com/mobarakali/Asiapac-Elementor-Slider
+ * Description: Display Elementor templates as slider slides via shortcode.
+ * Version: 1.0
+ * Author: Mobaraqk Ali
+ * Author URI: https://github.com/mobarakali/
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: asiapac-elementor-slider
+ */
+
+// Prevent direct access to the file
+
+if (!defined('ABSPATH')) {
+    exit; // Prevent direct access
+}
+
+// Enqueue Slick slider CSS and JS
+function aes_enqueue_slider_assets() {
+    wp_enqueue_style('slick-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
+    wp_enqueue_style('slick-theme-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css');
+    wp_enqueue_script('slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', ['jquery'], null, true);
+
+    // Slider Source  Slider GitHub URL
+   // https://kenwheeler.github.io/slick/
+
+
+    // Init script
+wp_add_inline_script('slick-js', "
+    jQuery(document).ready(function($){
+        $('.asiapac-elementor-slider').slick({
+            dots: true,
+            arrows: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            adaptiveHeight: true,
+            prevArrow: '<button type=\"button\" class=\"slick-prev \"></button>',
+            nextArrow: '<button type=\"button\" class=\"slick-next \"></button>'
+        });
+    });
+");
+    // Custom CSS for slider
+    wp_add_inline_style('slick-css', "
+        .asiapac-elementor-slider .slide {
+            width: 100%;
+            padding: 0;
+        }
+        .asiapac-elementor-slider .slick-arrow {
+            margin: 0  30px;
+            z-index: 9999;
+            height: 45px;
+            line-height: 1;
+            padding: 10px;
+            font-size: 20px;
+        }
+
+		.asiapac-elementor-slider .slick-prev:before,
+		.asiapac-elementor-slider .slick-next:before
+		{
+			font-family: 'slick';
+			font-size: 30px;
+			line-height: 1;
+			opacity: .75;
+			color: white;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+		}
+
+		.asiapac-elementor-slider [type=button],
+		.asiapac-elementor-slider button {
+			border: 0 solid #d60c11;
+		}
+		.asiapac-elementor-slider [type=button]:focus,
+		.asiapac-elementor-slider [type=button]:hover,
+		.asiapac-elementor-slider button:focus,
+		.asiapac-elementor-slider button:hover {
+			background-color: #d60c11;
+		}
+
+    ");
+
+}
+add_action('wp_enqueue_scripts', 'aes_enqueue_slider_assets');
+
+// Shortcode to display Elementor templates in a slider
+function aes_slider_shortcode($atts) {
+    $atts = shortcode_atts([
+        'ids' => '',
+    ], $atts);
+
+    if (empty($atts['ids'])) {
+        return '<p>No template IDs provided.</p>';
+    }
+
+    $ids = array_map('intval', explode(',', $atts['ids']));
+    $output = '<div class="asiapac-elementor-slider">';
+
+    foreach ($ids as $id) {
+        $output .= '<div class="slide">';
+        $output .= do_shortcode('[elementor-template id="' . $id . '"]');
+        $output .= '</div>';
+    }
+
+    $output .= '</div>';
+    return $output;
+}
+add_shortcode('asiapac_elementor_slider', 'aes_slider_shortcode');
